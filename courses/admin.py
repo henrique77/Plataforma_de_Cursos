17181b3224a0cb5df7c9fc9c6cbee8a7bbe5c0b4
henrique77 <henriquecaires@outlook.com>
2021-11-09 11:28:04 -0300

    Model admin e CRUD admin

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Course
+
+#Customizar o model no admin
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'start_date', 'created_at']
+    search_fields = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Course, CourseAdmin)
--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+class CourseManager(models.Manager):
+
+    def search(self, query):
+        return self.get_queryset().filter(
+            models.Q(name__icontains=query) | \
+            models.Q(description_icontains=query)
+        )
+
 class Course(models.Model):
     name = models.CharField('Nome', max_length=100)
     slug = models.SlugField('Atalho')
@@ -17,3 +25,14 @@ class Course(models.Model):
     updated_at = models.DateTimeField(
         'Atualizado em:', auto_now=True
     )
+
+    object = CourseManager()
+
+    #Representação de string com o nome do curso no model
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+        ordering = ['name']
