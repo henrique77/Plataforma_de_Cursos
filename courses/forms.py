890a6d835bef8ab31feec2b5066618bbc3ae9dd2
henrique77 <henriquecaires@outlook.com>
2021-11-10 11:43:11 -0300

    Form de contato do Curso

---
+++ courses/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+class ContactCourse(forms.Form):
+
+    name = forms.CharField(label='Nome', max_length=100)
+    email = forms.EmailField(label='E-mail')
+    message = forms.CharField(
+        label='Mensagem/Dúvida', widget=forms.Textarea
+    )
