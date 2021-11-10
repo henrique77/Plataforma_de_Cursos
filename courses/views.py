45d295b8589ea6e929a392031166434a883dd175
henrique77 <henriquecaires@outlook.com>
2021-11-10 12:37:14 -0300

    Form e template de contato do curso

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import Course
+from .forms import ContactCourse
 
 def index(request):
     courses = Course.object.all()
@@ -20,8 +21,13 @@ def index(request):
 
 def details(request, slug):
     course = get_object_or_404(Course, slug=slug)
+    if request.method == 'POST':
+        form = ContactCourse(request.POST)
+    else:
+        form = ContactCourse()
     context = {
-        'course': course
+        'course': course,
+        'form': form
     }
     template_name = 'courses/details.html'
     return render(request, template_name, context)
