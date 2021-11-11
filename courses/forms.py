2d5938ab47eda3a78f9905729d5585a9f568f32d
henrique77 <henriquecaires@outlook.com>
2021-11-11 09:50:54 -0300

    Update do envio de email com template

--- courses/forms.py
+++ courses/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.core.mail import send_mail
 from django.conf import settings
 
+from core.mail import send_mail_template
+
 class ContactCourse(forms.Form):
 
     name = forms.CharField(label='Nome', max_length=100)
@@ -12,14 +14,13 @@ class ContactCourse(forms.Form):
 
     def send_mail(self, course):
         subject = '[%s] Contato' % course
-        message = 'Nome: %(name)s; E-mail: %(email)s; %(message)s'
+        #message = 'Nome: %(name)s; E-mail: %(email)s; %(message)s'
         context = {
             'name': self.cleaned_data['name'],
             'email': self.cleaned_data['email'],
             'message': self.cleaned_data['message']
         }
-        message = message % context
-        send_mail(
-            subject, message, settings.DEFAULT_FROM_EMAIL,
-            [settings.CONTACT_EMAIL]
+        template_name = 'courses/contact_email.html'
+        send_mail_template(
+            subject, template_name, context, [settings.CONTACT_EMAIL]
         )
