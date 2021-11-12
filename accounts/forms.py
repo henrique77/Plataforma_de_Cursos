0cccdbc7e76b6ef5a39a7fd6e2e0dc00498e00a7
henrique77 <henriquecaires@outlook.com>
2021-11-12 11:18:56 -0300

    Aplicando filter para email unico do cadastro do usuario

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,16 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
 
     email = forms.EmailField(label='E-mail')
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Já existe usuário com este E-mail')
+        return email
 
     def save(self, commit=True):
         user = super(RegisterForm, self).save(commit=False)
