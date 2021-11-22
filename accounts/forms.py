d3bef9f0f43b21b81c280844ced875aaaee07309
henrique77 <henriquecaires@outlook.com>
2021-11-22 11:38:27 -0300

    Criação e ajustes na aplicação custom user

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,34 +1,34 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
-class RegisterForm(UserCreationForm):
+User = get_user_model()
 
-    email = forms.EmailField(label='E-mail')
+class RegisterForm(forms.ModelForm):
 
-    def clean_email(self):
-        email = self.cleaned_data['email']
-        if User.objects.filter(email=email).exists():
-            raise forms.ValidationError('Já existe usuário com este E-mail')
-        return email
+    password1 = forms.CharField(label='Senha', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirmação de Senha', widget=forms.PasswordInput)
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError('Aconfirmação de Senha não está correta')
+        return password2
 
     def save(self, commit=True):
         user = super(RegisterForm, self).save(commit=False)
-        user.email = self.cleaned_data['email']
+        user.set_password(self.cleaned_data['password1'])
         if commit:
             user.save()
         return user
 
-class EditAccountForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
 
-    def clean_email(self):
-        email = self.cleaned_data['email']
-        queryset = User.objects.filter\
-            (email=email).exclude(pk=self.instance.pk)
-        if queryset.exists():
-            raise forms.ValidationError('Já existe usuário com este E-mail')
-        return email
+class EditAccountForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name']
+        fields = ['username', 'email', 'name']
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,41 @@
-from django.db import models
+import re
 
-# Create your models here.
+from django.db import models
+from django.core import validators
+
+from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin,
+    UserManager)
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    username = models.CharField(
+        'Nome de Usuário', max_length=30, unique=True,
+        validators=[validators.RegexValidator(re.compile('^[\w.@+-]+$'),
+            'O nome do usuário só pode conter letras, digitos ou @/./+/-/_',
+            'invalid')]
+    )
+    email = models.EmailField('E-mail', unique=True)
+    name = models.CharField('Nome', max_length=100, blank=True)
+    is_active = models.BooleanField('Está ativo?', blank=True, default=True)
+    is_staff = models.BooleanField('É da equipe?', blank=True, default=False)
+    date_joined = models.DateTimeField('Data de Entrada', auto_now_add=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.name or self.username
+
+    def get_short_name(self):
+        return self.username
+
+    def get_full_name(self):
+        return str(self)
+
+    class Meta:
+        verbose_name = 'Usuário'
+        verbose_name_plural = 'Usuários'
+
+
